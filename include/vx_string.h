aca682bd25c8a5e7c5054f3950806dff6df25cd9
vsix <panagiotis.panteli.dev@gmail.com>
2026-05-06 14:19:05 +0300

    fs: implement cross-platform rmrf and refactor directory structure

--- include/vx_string.h
+++ include/vx_string.h
@@ -1,8 +1,10 @@
 #pragma GCC system_header
 #ifndef VX_STRING_H_
-#define VX_STRING_H_
+    #define VX_STRING_H_
 
-#include <string.h>
+    #include "vx_platform.h"
+    #include <string.h>
+    #include "vx_defs.h"
 
 static inline bool vx_strncmplit(const char *s, size_t len, const char *lit, size_t lit_len)
 {
@@ -13,5 +15,10 @@ static inline bool vx_strncmplit(const char *s, size_t len, const char *lit, siz
 
     return memcmp(s, lit, lit_len) == 0;
 }
+
+VX_API i32 vx_pathncmp(const char *s1, const char *s2, size_t n);
+
+VX_API void vx_fs_forbid_path(const char *path);
+VX_API bool vx_fs_is_path_protected(const char *target);
 
 #endif  // VX_STRING_H_
